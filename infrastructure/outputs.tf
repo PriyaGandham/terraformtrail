ebe8b816b75abd588eabf2dadb53b52e887b26e5
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-24 17:14:09 +0530

    Update appinsights_key output to be sensitive
    Mark application insights key as sensitive.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,7 +1,10 @@
 output "resource_group" { value = azurerm_resource_group.rg.name }
 output "storage_account" { value = azurerm_storage_account.sa.name }
 output "keyvault_uri" { value = azurerm_key_vault.kv.vault_uri }
-output "appinsights_key" { value = azurerm_application_insights.appi.instrumentation_key }
+output "appinsights_key" {
+  value     = azurerm_application_insights.appi.instrumentation_key
+  sensitive = true
+}
 output "azureai_endpoint" { value = azurerm_cognitive_account.azureai.endpoint }
 output "nesgen_model_id" {
   value = azurerm_cognitive_deployment.nesgen.id
