237b3ce68daaa5c1533ec54d5e5e4dda3562ceff
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-24 14:10:03 +0530

    Add output for nesgen model ID in outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,3 +3,6 @@ output "storage_account" { value = azurerm_storage_account.sa.name }
 output "keyvault_uri" { value = azurerm_key_vault.kv.vault_uri }
 output "appinsights_key" { value = azurerm_application_insights.appi.instrumentation_key }
 output "azureai_endpoint" { value = azurerm_cognitive_account.azureai.endpoint }
+output "nesgen_model_id" {
+  value = azurerm_cognitive_deployment.nesgen.id
+}
