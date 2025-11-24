660d2ae6cbedd9760a69668233f087417cbea1e2
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-24 12:17:45 +0530

    Update providers.tf
    added backend stage rg stg account details

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -9,6 +9,15 @@ terraform {
   required_version = ">= 1.5"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-backend-rg"
+    storage_account_name = "tfstatepgrstorage2025"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
