9dc95123ae429a143022d74ec5954bbb8a45ce47
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-24 15:48:27 +0530

    Add optional tags to key vault resources

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -7,10 +7,16 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
   soft_delete_retention_days  = 7
+
+  # Optional tags
+  tags = var.tags
 }
 
 resource "azurerm_key_vault_secret" "sample" {
   name         = "connection-string"
   value        = "sample-value"
   key_vault_id = azurerm_key_vault.kv.id
+
+  tags = var.tags
 }
+
