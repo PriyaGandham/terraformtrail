f3561f15d6551d63389294200b4c577e08962048
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-25 15:33:33 +0530

    Update app-insights.tf

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -4,8 +4,9 @@ resource "azurerm_application_insights" "appi" {
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
 
-  # IMPORTANT FIX
-  workspace_id = null
+  # IMPORTANT — existing workspace must remain
+  workspace_id        = "/subscriptions/054ee7c6-c56d-4907-909c-d82da0c2c1b4/resourceGroups/ai_appi-tf-devppp_b5a55b75-4681-4ba1-b388-79d2b566ca03_managed/providers/Microsoft.OperationalInsights/workspaces/managed-appi-tf-devPpP-ws"
 
   tags = var.tags
 }
+
