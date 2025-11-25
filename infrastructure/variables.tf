3aff36d5b440befbd354e64f2ee8c997d4f3dd0c
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-25 15:06:56 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,20 +5,23 @@ variable "kv_name" {}
 variable "appinsights_name" {}
 variable "ai_account_name" {}
 variable "nesgen_model_name" {}
+
 variable "ai_sku" {
-  description = "SKU for the Azure Cognitive Account "
+  description = "SKU for the Azure Cognitive Account"
   type        = string
   default     = "S0"
 }
+
 variable "tags" {
   type = map(string)
   default = {
     env = "dev"
   }
+}
+
 variable "create_cognitive_account" {
   type    = bool
-  default = true   # set false if you already have it and imported
-}
+  default = true   # Set false if you already imported the Cognitive Account
 }
 
 ##############################################################
@@ -32,4 +35,3 @@ variable "user_rbac_assignments" {
     role      = string
   }))
 }
-
