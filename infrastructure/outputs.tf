1c577cdf3abca4167d626fa914b5707d26516b0a
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-25 15:10:44 +0530

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -5,7 +5,23 @@ output "appinsights_key" {
   value     = azurerm_application_insights.appi.instrumentation_key
   sensitive = true
 }
-output "azureai_endpoint" { value = azurerm_cognitive_account.azureai.endpoint }
+output "azureai_endpoint" {
+  value = (
+    var.create_cognitive_account ?
+    azurerm_cognitive_account.azureai[0].endpoint :
+    null
+  )
+}
+
+output "azureai_key" {
+  value = (
+    var.create_cognitive_account ?
+    azurerm_cognitive_account.azureai[0].primary_access_key :
+    null
+  )
+  sensitive = true
+}
+
 output "nesgen_model_id" {
   value = azurerm_cognitive_deployment.nesgen.id
 }
