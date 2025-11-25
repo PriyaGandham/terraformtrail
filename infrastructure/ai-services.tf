b5d64cd3575c74ca39ae8f263606cc5b3f215bdb
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-25 14:52:30 +0530

    Update ai-services.tf

--- infrastructure/ai-services.tf
+++ infrastructure/ai-services.tf
@@ -1,4 +1,5 @@
 resource "azurerm_cognitive_account" "azureai" {
+  count               = var.create_cognitive_account ? 1 : 0
   name                = var.ai_account_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
