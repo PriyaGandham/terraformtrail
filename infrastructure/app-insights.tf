570355d224a4b2ab7cf9a24d7bba785eb9b286bf
priya <134846128+PriyaGandham@users.noreply.github.com>
2025-11-25 15:17:25 +0530

    Update app-insights.tf

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -3,5 +3,8 @@ resource "azurerm_application_insights" "appi" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
+
+  workspace_id = null
+
   tags                = var.tags
 }
